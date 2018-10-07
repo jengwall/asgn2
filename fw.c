fe8109922679d89af0b8e9d2017ff65f4f912772
Justin.Engwall <jengwall@calpoly.edu>
2018-10-07 11:10:34 -0700

    Take long list of characters

--- fw.c
+++ fw.c
@@ -1,28 +1,66 @@
-/* readword function was used from K&R p.20
- *
- */
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-void readword(FILE *fp);
+#define BUFSIZE 64
 
 struct word {
     char *string;
     int count;
 };
 
-/* Returns a sorted list, by frequency, of words in a file */
-int main() {
-    readword(stdin); 
-    /*get a file*/
-    /* read file looking for words */
+char *read_long_word(FILE *fp);
 
+int main() {
+    char *word;
+
+    while((word = read_long_word(stdin)) != NULL) {
+        if(word == NULL) {  /* error or end-of-file */
+           printf("size: %d\n", (int)sizeof(word));
+        }
+        else {
+            if((int)strlen(word) > 0) 
+                printf("%s\n", word);
+        }
+    }
     return 0;
 }
 
-/* readword: read a word from file or standard input */
-void readword(FILE *fp) {
-    int c;
-    while((c = getc(fp)) != EOF) {
-        printf("%c", (char)c);;
+/* read words from input */
+char *read_long_word(FILE *fp) {
+    char c;
+    int count, buf_size;
+    char *word;
+
+    count = 0;
+    buf_size = BUFSIZE;
+    word = calloc(buf_size, sizeof(char));
+    if (word == NULL) {
+        perror("Calloc failed");
+        exit(-1);
     }
+    while ((c = getc(fp)) != EOF) {
+        if (isalpha(c)) { /* test c for alphabetical char */
+            *(word + count) = c;    /* assign c to word buffer */
+            ++count;
+            if(count >= buf_size) {
+                buf_size += BUFSIZE; /* increment buffer */
+            word = (char *)realloc(word, buf_size * sizeof(char));
+            /* check to see if realloc fails */
+                if (word == NULL) {
+                    perror("Realloc failed");
+                    exit(-1);
+                }
+            }
+        } else { /* end of word */
+            *(word + count) = '\0';
+            return word;
+        }
+    }
+    return NULL;
 }
+
+/* sort words */
+
+/* print list */
