6995340ddc6c02f9734da80ec91478f6a9dfff24
Justin.Engwall <jengwall@calpoly.edu>
2018-10-07 12:46:10 -0700

    Seperate functions into multiple header and c files

--- fw.c
+++ fw.c
@@ -1,23 +1,20 @@
+/* Binary tree used from K&R example p.140 */
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include "addtree.h"
+#include "read_long_word.h"
 
-#define BUFSIZE 64
+void treeprint(struct tnode *);
 
-struct word {
-    char *string;
-    int count;
-};
-
-char *read_long_word(FILE *fp);
-
+/* sort words by frequency */
 int main() {
     char *word;
 
     while((word = read_long_word(stdin)) != NULL) {
         if(word == NULL) {  /* error or end-of-file */
-           printf("size: %d\n", (int)sizeof(word));
+           ;
         }
         else {
             if((int)strlen(word) > 0) 
@@ -26,41 +23,6 @@ int main() {
     }
     return 0;
 }
-
-/* read words from input */
-char *read_long_word(FILE *fp) {
-    char c;
-    int count, buf_size;
-    char *word;
-
-    count = 0;
-    buf_size = BUFSIZE;
-    word = calloc(buf_size, sizeof(char));
-    if (word == NULL) {
-        perror("Calloc failed");
-        exit(-1);
-    }
-    while ((c = getc(fp)) != EOF) {
-        if (isalpha(c)) { /* test c for alphabetical char */
-            *(word + count) = c;    /* assign c to word buffer */
-            ++count;
-            if(count >= buf_size) {
-                buf_size += BUFSIZE; /* increment buffer */
-            word = (char *)realloc(word, buf_size * sizeof(char));
-            /* check to see if realloc fails */
-                if (word == NULL) {
-                    perror("Realloc failed");
-                    exit(-1);
-                }
-            }
-        } else { /* end of word */
-            *(word + count) = '\0';
-            return word;
-        }
-    }
-    return NULL;
-}
-
 /* sort words */
 
 /* print list */
