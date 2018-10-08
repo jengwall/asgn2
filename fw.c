1df8f8b78b82e5e39a59497b360526fe7f727aa9
Justin.Engwall <jengwall@calpoly.edu>
2018-10-07 19:36:22 -0700

    Have a functional binary tree which lists nodes in lexagraphical order

--- fw.c
+++ fw.c
@@ -3,26 +3,32 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
+
 #include "addtree.h"
 #include "read_long_word.h"
-
-void treeprint(struct tnode *);
+#include "treeprint.h"
 
 /* sort words by frequency */
 int main() {
+    struct tnode *root;
     char *word;
 
-    while((word = read_long_word(stdin)) != NULL) {
-        if(word == NULL) {  /* error or end-of-file */
-           ;
-        }
-        else {
-            if((int)strlen(word) > 0) 
-                printf("%s\n", word);
-        }
-    }
-    return 0;
+    root = NULL;
+    while ((word = read_long_word(stdin)) != NULL)
+        if (isalpha(word[0]))
+            root = addtree(root, word);
+        treeprint(root);
+   return 0;
 }
 /* sort words */
 
+//    while((word = read_long_word(stdin)) != NULL) {
+//       if(word == NULL) {  /* error or end-of-file */
+//           ;
+//        }
+//        else {
+//            if((int)strlen(word) > 0) 
+//                printf("%s\n", word);
+//        }
+//    }
 /* print list */
